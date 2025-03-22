e56823cbd9e889b233d0c791253a9dfcfbfdaa13
SpongP41N7 <67070018@kmitl.ac.th>
2025-03-22 19:24:26 +0700

    what wrong

--- src/creditPage.java
+++ src/creditPage.java
@@ -137,7 +137,7 @@ public class creditPage {
             pFootage = new JPanel(); pTopage = new JPanel();
             pFootage.setOpaque(false); pTopage.setOpaque(false);
             pFootage.setPreferredSize(new Dimension(width, 80));
-            pTopage.setPreferredSize(new Dimension(width, 20));
+            pTopage.setPreferredSize(new Dimension(width, 30));
             
             pTop = new JPanel(new BorderLayout());
             pTop.setOpaque(false);
