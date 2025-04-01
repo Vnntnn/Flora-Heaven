431a2ab229cd13e26669d723665d412202ee9a39
FineNlow <thanabodee.nine@gmail.com>
2025-04-01 15:44:52 +0700

    Update AssetsLoaderBook.java

--- Flora Heaven/src/main/view/AssetsLoader/books/AssetsLoaderBook.java
+++ Flora Heaven/src/main/view/AssetsLoader/books/AssetsLoaderBook.java
@@ -4,10 +4,8 @@ package main.view.AssetsLoader.books;
  * @author Vnntnn
  */
 
-
 import javax.imageio.ImageIO;
 import javax.swing.*;
-import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
