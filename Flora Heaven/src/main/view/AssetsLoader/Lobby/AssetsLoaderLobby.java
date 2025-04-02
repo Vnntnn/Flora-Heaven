60dbbfeeb8503948a7d20667c61a0fbf62e194c4
Bxrtxrr <67070277@kmitl.ac.th>
2025-04-02 08:15:50 +0700

    Update AssetsLoaderLobby.java

--- Flora Heaven/src/main/view/AssetsLoader/Lobby/AssetsLoaderLobby.java
+++ Flora Heaven/src/main/view/AssetsLoader/Lobby/AssetsLoaderLobby.java
@@ -8,6 +8,7 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.net.URL;
 
 public class AssetsLoaderLobby {
     public static ImageIcon lobbygif;
@@ -20,9 +21,13 @@ public class AssetsLoaderLobby {
         try {
             Lobbybackground = ImageIO.read(AssetsLoaderLobby.class.getResourceAsStream("/assets/Lobby/background.png"));
             Gameboard = ImageIO.read(AssetsLoaderLobby.class.getResourceAsStream("/assets/Lobby/woodboard.png"));
-//            logo = ImageIO.read(AssetsLoaderLobby.class.getResourceAsStream("/assets/Lobby/logo/logo_new.png"));
 
-            gif = new ImageIcon(AssetsLoaderLobby.class.getResource("/Lobby/animations/logo_with_gif.GIF"));
+            URL imgURL = AssetsLoaderLobby.class.getResource("/assets/Lobby/animations/logo_with_gif.GIF");
+            if (imgURL != null) {
+                gif = new ImageIcon(imgURL);
+            } else {
+                System.err.println("Error: GIF not found!");
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
